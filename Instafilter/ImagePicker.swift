1676caa0924b969630c80d3b980cc787ad3a783c
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-06 23:51:52 +0100

    Progression

---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,56 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Edwin Przeźwiecki Jr. on 06/02/2023.
+//
+
+import Foundation
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            /// Tell the picker to go away:
+            picker.dismiss(animated: true)
+            
+            /// Exit if no selection was made:
+            guard let provider = results.first?.itemProvider else { return }
+            
+            /// If this has an image we can use, use it:
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+    }
+    
+    @Binding var image: UIImage?
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
